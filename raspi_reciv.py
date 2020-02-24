5c51393ff2b5812d44e5218c0e2fb866500a6b9f
ubutputra <ubut31@gmail.com>
2020-02-24 13:02:19 +0700

    yoi

---
+++ raspi_reciv.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+from lib_nrf24 import NRF24
+import time
+import spidev
+
+GPIO.setmode(GPIO.BCM)
+
+pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+
+radio = NRF24(GPIO, spidev.SpiDev())
+radio.begin(0, 17)
+
+radio.setPayloadSize(32)
+radio.setChannel(0x76)
+radio.setDataRate(NRF24.BR_1MBPS)
+radio.setPALevel(NRF24.PA_MIN)
+
+radio.setAutoAck(True)
+radio.enableDynamicPayloads()
+radio.enableAckPayload()
+
+radio.openWritingPipe(pipes[0])
+radio.openReadingPipe(1, pipes[1])
+radio.printDetails()
+# radio.startListening()
+
+message = list("GETSTRING")
+while len(message) &lt; 32:
+    message.append(0)
+
+while(1):
+    start = time.time()
+    radio.write(message)
+    print("Sent the message: {}".format(message))
+    radio.startListening()
---
+++ send_to_raspi/send_to_raspi.ino
@@ -0,0 +1,46 @@
+//SendReceive.ino
+
+#include<SPI.h>
+#include<RF24.h>
+
+// CE, CSN pins
+RF24 radio(9, 10);
+
+void setup(void){
+    while(!Serial);
+    Serial.begin(9600);
+
+    radio.begin();
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setChannel(0x76);
+    radio.openWritingPipe(0xF0F0F0F0E1LL);
+    const uint64_t pipe = (0xE8E8F0F0E1LL);
+    radio.openReadingPipe(1, pipe);
+
+    radio.enableDynamicPayloads();
+    radio.powerUp();
+
+}
+
+void loop(void){
+    radio.startListening();
+    Serial.println("Starting loop. Radio on.");
+    char receivedMessage[32] = {0};
+    if(radio.available()){
+        radio.read(receivedMessage, sizeof(receivedMessage));
+        Serial.println(receivedMessage);
+        Serial.println("Turning off the radio.");
+        radio.stopListening();
+
+        String stringMessage(receivedMessage);
+
+        if(stringMessage == "GETSTRING"){
+            Serial.println("Looks like they want a string!");
+            const char text[] = "Yo wassup, haha";
+            radio.write(text, sizeof(text));
+            Serial.println("We sent our message.");
+        }
+    }
+    delay(100);
+
+}
